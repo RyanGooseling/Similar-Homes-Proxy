094066df6a1b75b74fd7095640589370ea9f3905
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-10-08 17:32:24 -0400

    serving Andrew's API endpoints on server (some issues still with POST functionality)

--- server/index.js
+++ server/index.js
@@ -9,13 +9,40 @@ app.get('/', (req, res) => {
   res.send('this is a proxy server');
 });
 
+// ME
 app.get('/data/homes/:id', (req, res) => {
   axios({ method: 'get', 'url': `http://localhost:3001/data/homes/${req.params.id}`})
     .then((results) => {
       res.send(results.data);
     })
     .catch((err) => {
-      res.status(400).send('could not get data');
+      res.status(400).send('could not get similar homes data');
+    });
+});
+
+// ANDREW
+app.get('/homes/:houseId/schedule', (req, res) => {
+  axios({ method: 'get', url: `http://localhost:3004/homes/${req.params.houseId}/schedule`})
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      res.status(400).send('could not get schedule data');
+    });
+});
+
+app.post('/homes/:houseId/schedule', (req, res) => {
+  var options = {
+    method: 'get',
+    url: `http://localhost:3004/homes/${req.params.houseId}/schedule`,
+    data: req.body
+  };
+  axios(options)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      res.status(400).send('could not schedule new tour date');
     });
 });
 
